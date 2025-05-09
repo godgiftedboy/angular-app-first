12b881ea0a66d37d746df0601add35149b0bb391
godgiftedboy <rajprajapati12321@gmail.com>
2025-05-09 17:43:19 +0545

    chore: demo route with todo compoennt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
         path: '',
         pathMatch: 'full',
         loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
-    }
+    },
+    {
+        path: 'todos',
+        pathMatch: 'full',
+        loadComponent: () => import('./todos/todos.component').then(m => m.TodosComponent),
+    },
 ];
